5ce49fe60d466a1cebb3a6e4f5a88da00ce81f2a
Nick <nyaculak@gmail.com>
2012-02-13 21:12:15 -0500

    added more mapping

--- src/edu/wpi/first/wpilibj/templates/RoboMap.java
+++ src/edu/wpi/first/wpilibj/templates/RoboMap.java
@@ -5,6 +5,7 @@ package edu.wpi.first.wpilibj.templates;
  * @author Nick
  */
 public class RoboMap {
+    
     /* Digital */
     public static final int MOTOR1 = 1;
     public static final int MOTOR2 = 2;
@@ -13,8 +14,15 @@ public class RoboMap {
     public static final int LAUNCH_MOTOR = 5;
     public static final int LOAD_MOTOR = 6;
     public static final int BRIDGE_MOTOR = 7;
+    public static final int LAUNCH_LIMIT = 8;
+    public static final int LAUNCH_ENCODER1 = 9; //?
+    public static final int LAUNCH_ENCODER2 = 10; //?
     
     /* Analog */
+    public static final int AUTO_POT = 1;
+    public static final int TELO_POT = 2;
+    public static final int GYRO = 3;
+    public static final int BATTERY_METER = 4; //not actually used in code
     
     /* USB */
     public static final int JOYSTICK1 = 1;
