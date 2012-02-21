69d0d1a03cb2af5b2f96c027bc1973a437ec7945
Nick <nyaculak@gmail.com>
2012-02-21 16:19:48 -0500

    modified launching mechanics, added manual launch function

--- src/edu/wpi/first/wpilibj/templates/Launcher.java
+++ src/edu/wpi/first/wpilibj/templates/Launcher.java
@@ -23,15 +23,33 @@ public class Launcher {
         this.encoder = new Encoder(RoboMap.LAUNCH_ENCODER1, RoboMap.LAUNCH_ENCODER2, false);
         this.encoder.setDistancePerPulse(1);
     }
-    
-    private double getRPM() {
-        double encod1 = encoder.get();
-        double deltaTime = .01;
+
+        private double getRPM() {
+        double rate;
+        int sampleNumber = 500;
+        double deltaTime = .5;
+        double deltaEncod;
+        double[] encod1Array = new double[(sampleNumber + 1)];
+        double[] encod2Array = new double[(sampleNumber + 1)];
+        double encod1=0, encod2=0;
+        for(int i = 0; i < sampleNumber; i++) {
+            encod1Array[i] = encoder.getRaw();
+        }
         Timer.delay(deltaTime);
-        double encod2 = encoder.get();
-        double deltaEncod = encod2 - encod1;
-        double RPM = deltaEncod / (deltaTime * 6);
-        return RPM;
+        for(int i = 0; i < sampleNumber; i++) {
+            encod2Array[i] = encoder.getRaw();
+        }
+        for(int i = 0; i < sampleNumber; i++) {
+            encod1 += encod1Array[i];
+        }
+        encod1 /= (sampleNumber - 1);
+        for(int i = 0; i < sampleNumber; i++) {
+            encod2 += encod2Array[i];
+        }
+        encod2 /= (sampleNumber - 1);
+        deltaEncod = encod2 - encod1;
+        rate = deltaEncod / (deltaTime * 6) * 4.67;
+        return rate;
     }
     
     public void shoot(int pixel, double height) {
@@ -44,7 +62,14 @@ public class Launcher {
             Timer.delay(.025);
         }
         encoder.stop();
-        loadMotor.set(.25);
+        loadMotor.set(1);
+        Timer.delay(5);
+        loadMotor.set(0);
+        launchMotor.set(0);
+    }
+    
+    public void manualShoot() {
+        loadMotor.set(1);
         Timer.delay(5);
         loadMotor.set(0);
         launchMotor.set(0);
